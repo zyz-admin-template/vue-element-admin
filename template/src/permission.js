96ce6c8572e70c17cda56d3c9f7262df80953822
zhengyuze <1057195022@qq.com>
2020-07-10 13:55:05 +0800

    update

--- template/src/permission.js
+++ template/src/permission.js
@@ -1,74 +1,61 @@
-import router from './router'
-import store from './store'
-import { Message } from 'element-ui'
-import NProgress from 'nprogress' // progress bar
-import 'nprogress/nprogress.css' // progress bar style
-import { getToken } from '@/utils/auth' // get token from cookie
-import getPageTitle from '@/utils/get-page-title'
+import router from "./router";
+import store from "./store";
+import NProgress from "nprogress"; // progress bar
+import "nprogress/nprogress.css"; // progress bar style
+import { getToken } from "@/utils/auth"; // getToken from cookie
+import { buildMenus } from "@/api/login";
+import { filterAsyncRouter } from "./store/modules/permission";
 
-NProgress.configure({ showSpinner: false }) // NProgress Configuration
+NProgress.configure({ showSpinner: false }); // NProgress Configuration
 
-const whiteList = ['/login', '/auth-redirect'] // no redirect whitelist
+const whiteList = ["/login"]; // no redirect whitelist
 
-router.beforeEach(async(to, from, next) => {
-  // start progress bar
-  NProgress.start()
-
-  // set page title
-  document.title = getPageTitle(to.meta.title)
-
-  // determine whether the user has logged in
-  const hasToken = getToken()
-
-  if (hasToken) {
-    if (to.path === '/login') {
-      // if is logged in, redirect to the home page
-      next({ path: '/' })
-      NProgress.done()
+router.beforeEach((to, from, next) => {
+  NProgress.start(); // start progress bar
+  if (getToken()) {
+    // 已登录且要跳转的页面是登录页
+    if (to.path === "/login") {
+      next({ path: "/" });
+      NProgress.done(); // if current page is dashboard will not trigger	afterEach hook, so manually handle it
     } else {
-      // determine whether the user has obtained his permission roles through getInfo
-      const hasRoles = store.getters.roles && store.getters.roles.length > 0
-      if (hasRoles) {
-        next()
+      if (store.getters.roles.length === 0) {
+        // 判断当前用户是否已拉取完user_info信息
+        store
+          .dispatch("GetInfo")
+          .then(res => {
+            // 拉取user_info
+            buildMenus().then(res => {
+              const asyncRouter = filterAsyncRouter([] || res.detail);
+              asyncRouter.push({ path: "*", redirect: "/404", hidden: true });
+              store.dispatch("GenerateRoutes", asyncRouter).then(() => {
+                // 存储路由
+                router.addRoutes(asyncRouter); // 动态添加可访问路由表
+                next({ ...to, replace: true }); // hack方法 确保addRoutes已完成
+              });
+            });
+          })
+          .catch(err => {
+            console.log(err);
+            store.dispatch("LogOut").then(() => {
+              location.reload(); // 为了重新实例化vue-router对象 避免bug
+            });
+          });
       } else {
-        try {
-          // get user info
-          // note: roles must be a object array! such as: ['admin'] or ,['developer','editor']
-          const { roles } = await store.dispatch('user/getInfo')
-
-          // generate accessible routes map based on roles
-          const accessRoutes = await store.dispatch('permission/generateRoutes', roles)
-
-          // dynamically add accessible routes
-          router.addRoutes(accessRoutes)
-
-          // hack method to ensure that addRoutes is complete
-          // set the replace: true, so the navigation will not leave a history record
-          next({ ...to, replace: true })
-        } catch (error) {
-          // remove token and go to login page to re-login
-          await store.dispatch('user/resetToken')
-          Message.error(error || 'Has Error')
-          next(`/login?redirect=${to.path}`)
-          NProgress.done()
-        }
+        next();
       }
     }
   } else {
     /* has no token*/
-
     if (whiteList.indexOf(to.path) !== -1) {
-      // in the free login whitelist, go directly
-      next()
+      // 在免登录白名单，直接进入
+      next();
     } else {
-      // other pages that do not have permission to access are redirected to the login page.
-      next(`/login?redirect=${to.path}`)
-      NProgress.done()
+      next(`/login?redirect=${to.path}`); // 否则全部重定向到登录页
+      NProgress.done();
     }
   }
-})
+});
 
 router.afterEach(() => {
-  // finish progress bar
-  NProgress.done()
-})
+  NProgress.done(); // finish progress bar
+});
--- template/src/store/modules/permission.js
+++ template/src/store/modules/permission.js
@@ -1,69 +1,50 @@
-import { asyncRoutes, constantRoutes } from '@/router'
+import { constantRouterMap } from "@/router";
+import Layout from "@/views/layout/Layout";
 
-/**
- * Use meta.role to determine if the current user has permission
- * @param roles
- * @param route
- */
-function hasPermission(roles, route) {
-  if (route.meta && route.meta.roles) {
-    return roles.some(role => route.meta.roles.includes(role))
-  } else {
-    return true
-  }
-}
+const permission = {
+  state: {
+    routers: constantRouterMap,
+    addRouters: [],
+  },
+  mutations: {
+    SET_ROUTERS: (state, routers) => {
+      state.addRouters = routers;
+      state.routers = constantRouterMap.concat(routers);
+      console.log('最后',state.routers);
+    },
+  },
+  actions: {
+    GenerateRoutes({ commit }, asyncRouter) {
+      console.log('当前用户可看菜单',asyncRouter);
+      commit("SET_ROUTERS", asyncRouter);
+    },
+  },
+};
 
-/**
- * Filter asynchronous routing tables by recursion
- * @param routes asyncRoutes
- * @param roles
- */
-export function filterAsyncRoutes(routes, roles) {
-  const res = []
-
-  routes.forEach(route => {
-    const tmp = { ...route }
-    if (hasPermission(roles, tmp)) {
-      if (tmp.children) {
-        tmp.children = filterAsyncRoutes(tmp.children, roles)
-      }
-      res.push(tmp)
-    }
-  })
-
-  return res
-}
-
-const state = {
-  routes: [],
-  addRoutes: []
-}
-
-const mutations = {
-  SET_ROUTES: (state, routes) => {
-    state.addRoutes = routes
-    state.routes = constantRoutes.concat(routes)
-  }
-}
-
-const actions = {
-  generateRoutes({ commit }, roles) {
-    return new Promise(resolve => {
-      let accessedRoutes
-      if (roles.includes('admin')) {
-        accessedRoutes = asyncRoutes || []
+export const filterAsyncRouter = (routers) => {
+  // 遍历后台传来的路由字符串，转换为组件对象
+  console.log(routers);
+  const accessedRouters = routers.filter((router) => {
+    if (router.component) {
+      if (router.component === "Layout") {
+        // Layout组件特殊处理
+        router.component = Layout;
       } else {
-        accessedRoutes = filterAsyncRoutes(asyncRoutes, roles)
+        const component = router.component;
+        router.component = loadView(component);
       }
-      commit('SET_ROUTES', accessedRoutes)
-      resolve(accessedRoutes)
-    })
-  }
-}
+    }
+    if (router.children && router.children.length) {
+      router.children = filterAsyncRouter(router.children);
+    }
+    return true;
+  });
+  return accessedRouters;
+};
 
-export default {
-  namespaced: true,
-  state,
-  mutations,
-  actions
-}
+export const loadView = (view) => {
+  // 路由懒加载
+  return () => import(`@/views/${view}`);
+};
+
+export default permission;
